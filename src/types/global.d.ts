22d60444cca89e39e0aaefb4d03f1bc58dfde43d
moncef <epiccoder00@gmail.com>
2023-11-29 17:27:37 +0100

    added project type

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,13 +1,29 @@
-type Article = {
-    slug: string;
-    frontmatter: {
-      image: string;
-      title: string;
-      description: string;
-      date: string;
-      author: string;
-      author_skill: string;
-      author_image: string;
-    };
-  }
-  
+import { Locale } from "@/i18n.config";
+
+export type Article = {
+  slug: string;
+  frontmatter: {
+    lang: Locale;
+    image: string;
+    title: string;
+    description: string;
+    date: string;
+    author: string;
+    author_skill: string;
+    author_image: string;
+  };
+};
+
+export type Project = {
+  slug: string;
+  frontmatter: {
+    lang: Locale;
+    image: string;
+    title: string;
+    description: string;
+    date: string;
+    link: string;
+    tech: string;
+    color: string;
+  };
+};
