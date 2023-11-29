e5c2f22bb93b66423f47d59c481a3d75b716a6fe
moncef <epiccoder00@gmail.com>
2023-11-29 01:22:34 +0100

    added  type for the blog article

---
+++ src/types/global.d.ts
@@ -0,0 +1,13 @@
+type Article = {
+    slug: string;
+    frontmatter: {
+      image: string;
+      title: string;
+      description: string;
+      date: string;
+      author: string;
+      author_skill: string;
+      author_image: string;
+    };
+  }
+  
