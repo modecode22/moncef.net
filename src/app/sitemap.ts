65f966b709d7ca8c72efb0fecd544fec6a36f934
moncef <epiccoder00@gmail.com>
2023-11-30 23:28:04 +0100

    fixed some issues in the sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,8 +5,9 @@ const URL = "https://moncef.net";
 
 export default async function sitemap() {
   const mdDirectory = path.join(process.cwd(), "public");
-  const files = fs.readdirSync(mdDirectory + "/posts/");
-  const posts = await files.map((fileName) => {
+  const blogFiles =  fs.readdirSync(mdDirectory + "/posts/");
+  const projectsFiles =  fs.readdirSync(mdDirectory + "/projects/");
+  const posts =  blogFiles.map((fileName) => {
     const slug = fileName.replace(".mdx", "");
     const readFile = fs.readFileSync(
       mdDirectory + `/posts/${fileName}`,
@@ -19,13 +20,26 @@ export default async function sitemap() {
       lastModified: new Date(frontmatter.date).toISOString(),
     };
   });
+  const projects =  projectsFiles.map((fileName) => {
+    const slug = fileName.replace(".mdx", "");
+    const readFile = fs.readFileSync(
+      mdDirectory + `/projects/${fileName}`,
+      "utf-8"
+    );
+    const { data: frontmatter } = matter(readFile);
 
-  const routes = ["", "/blog", "about-us", "services", "portfolio"].map(
+    return {
+      url: `${URL}/projects/${slug}`,
+      lastModified: new Date(frontmatter.date).toISOString(),
+    };
+  });
+
+  const routes = ["", "/blog", "/contact", "/projects"].map(
     (route) => ({
       url: `${URL}${route}`,
       lastModified: new Date().toISOString(),
     })
   );
 
-  return [...routes, ...posts];
+  return [...routes, ...posts,...projects];
 }
