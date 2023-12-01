e0878a56f61c67680c9d8173055689da642e4281
moncef <epiccoder00@gmail.com>
2023-12-01 23:22:56 +0100

    started working on the main page

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,9 +1,18 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-
+    <main className="flex flex-col    items-center  min-h-[80vh] w-full   gap-5">
+<h1 className="text-4xl font-bold">Welcome</h1>
+<h1 className="text-6xl font-bold">I'm Moncef</h1>
+<h1 className="text-6xl font-bold">Full-Stack</h1>
+<h1 className="text-6xl font-bold">Developer</h1>
+<h1 className="text-6xl font-bold">From</h1>
+<section className="flex text-6xl font-bold">
+<h2 className="text-primary-500">Alg</h2>
+<h2 className="text-red-500">e</h2>
+<h2 className="text-light-500">ria</h2>
+</section>
     </main>
-  )
+  );
 }
